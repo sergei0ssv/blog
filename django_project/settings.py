7f5228a953fd035982a557b8b5368e8f30a5400e
Sergei Sudakov <sudakov.tomsk@gmail.com>
2024-05-31 21:03:59 +0700

    attempt to overcome login problem on amvera

--- django_project/settings.py
+++ django_project/settings.py
@@ -123,3 +123,6 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_TRUSTED_ORIGINS = ['https://*.blog-sergei0ssv.amvera.io','https://*.127.0.0.1']
