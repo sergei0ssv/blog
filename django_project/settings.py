9f59b4c96cf5631e446dfa85146bfc4205065447
Sergei Sudakov <sudakov.tomsk@gmail.com>
2024-05-31 20:10:39 +0700

    changes for deploying to amvera

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&&ktr%cx#z^v3dcp04vmlpcw!xs&wro4w8#_&q*wx%-4cxba&d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
